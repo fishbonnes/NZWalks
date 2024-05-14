e35a4bf5b61717327e6c062c8650cc887aa9984e
fishbonnes <fishbonnes@gmail.com>
2024-05-14 14:58:04 +0100

    End of Section 2

---
+++ NZWalks2/NZWalks2.API/Data/NZWalks2DBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks2.API.Models.Domain;
+
+namespace NZWalks2.API.Data
+{
+    public class NZWalks2DBContext: DbContext
+    {
+        public NZWalks2DBContext(DbContextOptions<NZWalks2DBContext> options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
--- NZWalks2/NZWalks2.API/Program.cs
+++ NZWalks2/NZWalks2.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks2.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NZWalks2DBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks2"));
+});
 
 var app = builder.Build();
 
