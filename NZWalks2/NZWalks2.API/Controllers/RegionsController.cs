9c1b0a557fe0906698af26c67ab93c9dd7ef43c4
fishbonnes <fishbonnes@gmail.com>
2024-05-16 10:53:22 +0100

    End of Section 7

--- NZWalks2/NZWalks2.API/Controllers/RegionsController.cs
+++ NZWalks2/NZWalks2.API/Controllers/RegionsController.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
 using NZWalks2.API.Models.Domain;
 using NZWalks2.API.Models.DTO;
 using NZWalks2.API.Repositories;
+using System.Linq;
 
 namespace NZWalks2.API.Controllers
 {
@@ -62,6 +65,12 @@ namespace NZWalks2.API.Controllers
         [HttpPost]
         public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest) 
         {
+            // Validate the request
+            if (!ValidateAddRegionAsync(addRegionRequest))
+            {
+                return BadRequest(ModelState);
+            };
+
             // Request(DTO) to Domain model
             var region = new Models.Domain.Region()
             {
@@ -121,6 +130,12 @@ namespace NZWalks2.API.Controllers
         [Route("{id:guid}")]
         public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id,[FromBody] Models.DTO.UpdateRegionRequest updateRegionRequest)
         {
+            // Validate the incoming request
+            if(!ValidateUpdateRegionAsync(updateRegionRequest))
+            {
+                return BadRequest(ModelState);
+            }
+
             // Convert DTO to Domain model
             var region = new Models.Domain.Region()
             {
@@ -152,5 +167,74 @@ namespace NZWalks2.API.Controllers
             //Return Ok Response
             return Ok(regionsDTO);
         }
+        #region Private methods
+
+        private bool ValidateAddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
+        {
+            
+            if (addRegionRequest == null)
+            {
+                ModelState.AddModelError(nameof(addRegionRequest), $"Add Region Data is required.");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(addRegionRequest.Code))
+            {
+                ModelState.AddModelError(nameof(AddRegionRequest.Code), $"{nameof(AddRegionRequest.Code)} cannot be null or empty or white space");
+            }
+
+            if (string.IsNullOrWhiteSpace(addRegionRequest.Name))
+            {
+                ModelState.AddModelError(nameof(AddRegionRequest.Name), $"{nameof(AddRegionRequest.Name)} cannot be null or empty or white space");
+            }
+            if (addRegionRequest.Area <= 0)
+            {
+                ModelState.AddModelError(nameof(addRegionRequest.Area), $"{nameof(addRegionRequest.Area)} cannot be less or equal to zero");
+            }
+            
+            if (addRegionRequest.Population < 0)
+            {
+                ModelState.AddModelError(nameof(addRegionRequest.Population), $"{nameof(addRegionRequest.Population)} cannot be less than zero");
+            }
+            if(ModelState.ErrorCount > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateUpdateRegionAsync(Models.DTO.UpdateRegionRequest updateRegionRequest)
+        {
+            if (updateRegionRequest == null)
+            {
+                ModelState.AddModelError(nameof(updateRegionRequest), $"Add Region Data is required.");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(updateRegionRequest.Code))
+            {
+                ModelState.AddModelError(nameof(updateRegionRequest.Code), $"{nameof(updateRegionRequest.Code)} cannot be null or empty or white space");
+            }
+
+            if (string.IsNullOrWhiteSpace(updateRegionRequest.Name))
+            {
+                ModelState.AddModelError(nameof(updateRegionRequest.Name), $"{nameof(updateRegionRequest.Name)} cannot be null or empty or white space");
+            }
+            if (updateRegionRequest.Area <= 0)
+            {
+                ModelState.AddModelError(nameof(updateRegionRequest.Area), $"{nameof(updateRegionRequest.Area)} cannot be less or equal to zero");
+            }
+            if (updateRegionRequest.Population < 0)
+            {
+                ModelState.AddModelError(nameof(updateRegionRequest.Population), $"{nameof(updateRegionRequest.Population)} cannot be less than zero");
+            }
+            if (ModelState.ErrorCount > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
     }
+
 }
--- NZWalks2/NZWalks2.API/Controllers/WalkDifficultyController.cs
+++ NZWalks2/NZWalks2.API/Controllers/WalkDifficultyController.cs
@@ -54,6 +54,11 @@ namespace NZWalks2.API.Controllers
         [HttpPost]
         public async Task<IActionResult> AddWalkDifficultyAsinc(Models.DTO.AddWalkDifficultyRequest addWalkDifficultyRequest)
         {
+            // Validate the request
+            if(!ValidateAddWalkDifficultyAsinc(addWalkDifficultyRequest))
+            {
+                return BadRequest(ModelState);
+            }
             // Request(DTO) to Domain model
             var walkDifficulty = new Models.Domain.WalkDifficulty()
             {
@@ -75,6 +80,12 @@ namespace NZWalks2.API.Controllers
         [Route("{id:guid}")]
         public async Task<IActionResult> UpdateWalkDifficultyAsync([FromRoute] Guid id, [FromBody] Models.DTO.UpdateWalkDifficultyRequest updateWalkDifficultyRequest)
         {
+            // Validate the request
+            if (!ValidateUpdateWalkDifficultyAsync(updateWalkDifficultyRequest))
+            {
+                return BadRequest(ModelState) ;
+            }
+
             // Convert DTO to Domain model
             var walkDifficulty = new Models.Domain.WalkDifficulty()
             {
@@ -115,5 +126,52 @@ namespace NZWalks2.API.Controllers
             };
             return Ok(walkDifficultyDTO);
         }
+        #region Private methods
+
+        private  bool ValidateAddWalkDifficultyAsinc(Models.DTO.AddWalkDifficultyRequest addWalkDifficultyRequest)
+        {
+            if (addWalkDifficultyRequest == null)
+            {
+                ModelState.AddModelError(nameof(addWalkDifficultyRequest), $"Add WalkDifficulty Data is required.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(addWalkDifficultyRequest.Code))
+            {
+                ModelState.AddModelError(nameof(addWalkDifficultyRequest.Code), $"{nameof(addWalkDifficultyRequest.Code)}is required.");
+
+            }
+
+            if (ModelState.ErrorCount > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateUpdateWalkDifficultyAsync(Models.DTO.UpdateWalkDifficultyRequest updateWalkDifficultyRequest)
+        {
+            if (updateWalkDifficultyRequest == null)
+            {
+                ModelState.AddModelError(nameof(updateWalkDifficultyRequest), $"Add WalkDifficulty Data is required.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(updateWalkDifficultyRequest.Code))
+            {
+                ModelState.AddModelError(nameof(updateWalkDifficultyRequest.Code), $"{nameof(updateWalkDifficultyRequest.Code)}is required.");
+
+            }
+
+            if (ModelState.ErrorCount > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
     }
 }
